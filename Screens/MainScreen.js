cfde11b36710cde37f0964a031f79bedf942352b
Malik_Kaleem <malikkaleem171@gmail.com>
2021-03-14 15:11:40 +0500

    login pages

---
+++ Screens/MainScreen.js
@@ -0,0 +1,20 @@
+import React,{useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+ 
+} from "react-native";
+export default MainScreen=()=>{
+ return(
+     <>
+     </>
+
+    )
+}
+
