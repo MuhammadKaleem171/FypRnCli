7aa69d1df14d67b98c7a4cc7c949f2011e42b16d
Malik_Kaleem <malikkaleem171@gmail.com>
2021-04-16 15:16:30 +0500

    Where & orderBy

--- Screens/Student/QScreen/OrderBy.js
+++ Screens/Student/QScreen/OrderBy.js
@@ -23,6 +23,13 @@ import {
     ServerName:'MALIKKALEEM\SQLEXPRESS01'
   }
   ]
+
+  const OderType=[{
+    type:'DESC'
+  },
+{
+  type:'ASC'
+}]
 const OrderBy=(props)=>{
     const[Database,setDatabase]=useState([])
     const[SelectedDatabase,setSelectedDatabase]=useState()
@@ -41,12 +48,182 @@ const OrderBy=(props)=>{
     const [WhereClause,setWhereClause]=useState(false)
     const [OrderByColumn,setOrderByColum]=useState('')
 
+    const [OrderByType,setOrderByType]=useState('')
+    const[WhereColumn,setWhereColumn]=useState('')
+    const[condition,setCondition]=useState()
+    const [conditionValue,setConditionValue]=useState()
+    // -----------------Function ----------
+
+    useEffect(() => {
+      fetch('http://192.168.10.6/backend/api/values/GetDatabase')
+      .then(res=>res.json())
+      .then((data)=>{
+          setDatabase(data)
+          console.log(data);
+      });            
+    }, []);
+
+    
+   const GetTabeName=(item)=>{
+     const database=item.itemValue
+    fetch(`http://192.168.10.6/backend/api/values/gettable?TableName=${database}`)
+    .then(res=>res.json())
+    .then((data)=>{
+        console.log(data)
+        setTableName(data)   
+       
+    });
+    }
+   
+const GetColumnNames=(da)=>{
+console.log('table name name',da.itemValue)
+const data=da.itemValue
+      fetch(`http://192.168.10.6/backend/api/values/GetTableColumn?table=${data}&DatabaseName=${SelectedDatabase}`)
+.then(res=>res.json())
+.then((data)=>{
+    //console.log(data)
+    setCoulumnName(data)
+});
+    }
+
+
+ const co=()=>{
+  let a='';
+  ColumnName.forEach(element => {
+    if(element.isChecked==true){      
+        console.log(element.id,element.column)
+         a=a+element.column+','   
+    }
+    
+});
+let v=''
+  for(let i=0;i<a.length-1;i++){
+    v=v+a[i]
+  }
+  
+  
+setQColum(v);
+
+  }
+  
+ const ShowQuery=()=>{
+  let query=""
+   if(OrderBy){
+   query=QueryType +' '+QColum+' '+'from'+' '+SelectedTable+' Order by '+OrderByColumn +' '+OrderByType
+   }
+   else if(WhereClause){
+    query=QueryType +' '+QColum+' '+'from'+' '+SelectedTable+' Where '+WhereColumn +' '+condition +' '+conditionValue
+
+   }
+  setQuery(query)
+   
+ }
+
+ const GetqueryFromDatabase=()=>{
+   console.log('clicked')
+   fetch(`http://192.168.10.6/backend/api/values/SaveQuery?UserName=17-arid-3460`)
+   .then(res=>res.json())
+   .then((response)=>{
+     console.log(response)
+     setGetSavedQuery(response)
+   }).catch((error)=>console.log('erroe',error))
+ }
+
+ const PostSavedQuery=()=>{
+   console.log('post')
+
+  fetch('http://192.168.10.6/backend/api/Values/PostQuery', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      UserName: "17-ardi-3461",
+        Query:query,
+        DatabaseName:SelectedDatabase,
+        Query_Name:Query_Name
+    }),
+  }).then(response => response.json()) 
+  .then(json => {
+    console.log(json);
+    alert(json)
+  })
+  
+ }
+
     const OderBYF = () => setOrderBy(previousState => !previousState);
     const WhereClauseF = () => setWhereClause(previousState => !previousState);
 
     return(
         <View style={{flex:1,backgroundColor:'#fff'}}>
             <ScrollView>
+              {/* <-------------------------- Order by ------------------------> */}
+            <View style={{marginTop:10}}>
+              <Text style={styles.intro}>
+              The ORDER BY keyword is used to sort the result-set by a specified column.
+              {"\n"}
+              </Text>
+              
+              <Text style={styles.syntax}>
+              SQL ORDER BY Syntax
+              </Text>
+              <Text>
+              SELECT column_name(s) FROM table_name ORDER BY column_name(s) ASC|DESC
+              </Text>
+              </View>
+              {/* <---------------------------Examples---------------------->              */}
+              <View>
+                <View>
+                  <Text style={styles.syntax}> Examples</Text>
+                </View>
+                <View style={{flex:1,flexDirection:'column',alignItems:'center'}}>
+                  <View style = {{ width:'100%', height:250,display:'flex',borderWidth:1,overflow:'hidden',marginBottom:10}}>
+                    <Image source={require("../../../assets/Examples/Select1.jpg")}  resizeMode='contain' style={{width:'100%',height:250}} />
+                  </View>
+                  <View style = {{ width:'100%', height:250,display:'flex',borderWidth:1,overflow:'hidden',marginBottom:10}}>
+                    <Image source={require("../../../assets/Examples/Select2.jpg")} resizeMode='contain' style={{width:'100%',height:250}} />
+                  </View>
+                  <View style = {{ width:'100%', height:250,display:'flex',borderWidth:1,overflow:'hidden',marginBottom:10}}>
+                    <Image source={require("../../../assets/Examples/Select3.jpg")} resizeMode='contain' style={{width:'100%',height:250}}/>
+                  </View>
+                </View>
+              </View>
+{/* <--------------------------------------------------Where clause -----------------------> */}
+<View style={{marginTop:10}}>
+              <Text style={styles.intro}>
+              The WHERE clause is used to filter records.
+              {"\n"}
+              </Text>
+              
+              <Text style={styles.syntax}>
+              SQL WHERE Syntax
+              </Text>
+              <Text>
+              SELECT column_name(s)FROM table_name WHERE column_name operator value
+              </Text>
+              </View>
+              {/* <---------------------------Examples---------------------->              */}
+              <View>
+                <View>
+                  <Text style={styles.syntax}> Examples</Text>
+                </View>
+                <View style={{flex:1,flexDirection:'column',alignItems:'center'}}>
+                  <View style = {{ width:'100%', height:250,display:'flex',borderWidth:1,overflow:'hidden',marginBottom:10}}>
+                    <Image source={require("../../../assets/Examples/Select1.jpg")}  resizeMode='contain' style={{width:'100%',height:250}} />
+                  </View>
+                  <View style = {{ width:'100%', height:250,display:'flex',borderWidth:1,overflow:'hidden',marginBottom:10}}>
+                    <Image source={require("../../../assets/Examples/Select2.jpg")} resizeMode='contain' style={{width:'100%',height:250}} />
+                  </View>
+                  <View style = {{ width:'100%', height:250,display:'flex',borderWidth:1,overflow:'hidden',marginBottom:10}}>
+                    <Image source={require("../../../assets/Examples/Select3.jpg")} resizeMode='contain' style={{width:'100%',height:250}}/>
+                  </View>
+                </View>
+              </View>
+
+
+
+
             <View style={{flex:1,flexDirection:'row',marginTop:10,justifyContent:"space-evenly"}}>
             <View  style={{display:'flex',flexDirection:'row',height:60,marginLeft:10}}>
     <View>
@@ -209,6 +386,21 @@ const OrderBy=(props)=>{
           
       </View>
       <View>
+      <Picker style={styles.dataBasePiker}
+             dropdownIconColor="#21130d" 
+             mode="dropdown"
+            selectedValue={OrderByType}
+            onValueChange={OrderByType=>setOrderByType(OrderByType)}
+            >
+            {
+              OderType.map((data,i)=>{
+                return(
+                  <Picker.Item key={i} label={data.type} value={data.type}/>
+                )
+              })
+            }
+
+            </Picker>
           
       </View>
 
@@ -232,10 +424,154 @@ const OrderBy=(props)=>{
 
 <View>
     {WhereClause ?
-    <View>
-
-<Text> wheerreeeeeeeeeeee</Text>
+  <View>
+  <View style={styles.DatabaseView}>
+              <Text style={styles.heading1}>Select Server Name</Text>
+              <Picker style={styles.dataBasePiker}
+               dropdownIconColor="#21130d" 
+               mode="dropdown"
+              selectedValue={"MALIKKALEEM\SQLEXPRESS01"}
+              >
+              {
+                serverList.map((data,i)=>{
+                  return(
+                    <Picker.Item key={i} label={data.ServerName} value={data.ServerName}/>
+                  )
+                })
+              }
+  
+              </Picker>
+              
+              </View>
+              <View style={styles.DatabaseView}>
+              <Text style={styles.heading1}>Select the Database</Text>
+            <Picker style={styles.dataBasePiker}
+        dropdownIconColor="#21130d" 
+        mode="dropdown"
+    selectedValue={SelectedDatabase}
+    onValueChange={((itemValue, itemIndex)=>{
+      setSelectedDatabase(itemValue)
+      GetTabeName({itemValue})
+    }
+    )
+    }>
+      {
+        Database.map(data=>{
+          return(  <Picker.Item key={data} label={data} value={data} />)
+        })
+      }
+  
+   
+  </Picker>
+  </View>
+  
+  {/*  Select table View    */}
+  <View style={styles.TableView} >
+  
+  <Text style={styles.heading1}>Select Table Name </Text>
+  <Picker style={styles.dataBasePiker}
+   dropdownIconColor="#21130d" 
+   mode="dropdown"
+    selectedValue={SelectedTable}
+    onValueChange={((itemValue, itemIndex)=>{
+      setSelecteTable(itemValue)
+      GetColumnNames({itemValue})
+    }
+    )
+    }>
+      {
+        TableName.map(data=>{
+          return(  <Picker.Item key={data} label={data} value={data} />)
+        })
+      }
+  
+   
+  </Picker>
+  </View>
+  
+  <View > 
+  <Text style={styles.heading1}>Select column's </Text>
+    {
+      ColumnName.length>=1?
+      ColumnName.map(data=>{
+       // console.log(data)
+        return(
+          <View key={data.id} style={styles.ColumView}>
+            <CheckBox 
+            value={data.isChecked}
+          onValueChange={e=>{
+            setCoulumnName(ColumnName.map(d=>{
+              if(d.id==data.id){
+                d.isChecked=!data.isChecked
+              }
+              return d;
+            }))
+            co()
+          }
+        
+        }
+            />
+            <Text>{data.column}</Text>
+            </View>
+        )
+      }):null
+    }
     </View>
+  
+    {/* <-------------------------Where condition handle------------------------> */}
+    <View>
+        <View>
+            
+            <Text style={styles.heading1}>Select Column For Condition </Text>
+            
+        </View>
+        <Picker style={styles.dataBasePiker}
+  selectedValue={WhereColumn}
+  onValueChange={((itemValue, itemIndex)=>{
+    console.log(itemValue)
+    setWhereColumn(itemValue)
+  }
+  )
+  }>
+    { 
+      ColumnName.map((item,id)=>{
+        return(  <Picker.Item  key={item.id} label={item.column} value={item.column} />)
+      })
+    }
+</Picker>
+<Text style={styles.heading1}> Select Condition</Text>
+<Picker selectedValue={condition} 
+    style={styles.dataBasePiker}
+    onValueChange={(item,index)=>{
+      setCondition(item)
+      console.log(item)
+    }}>
+      <Picker.Item label=">" value=">" />
+      <Picker.Item label="<" value="<"/>
+      <Picker.Item label="=" value="="/>
+    </Picker>
+  <View>
+  <TextInput 
+    value={conditionValue}
+    placeholder="enter Condition Value"
+    style={styles.ColumnTextView} style={{borderWidth:1,height:40,marginLeft:15}}
+    onChangeText={(condition) => setConditionValue(condition)}    />
+    </View>
+  
+    </View>
+   
+  
+  <View style={{height:150,marginTop:20,justifyContent:'center'}}>
+  <Button onPress={() => {
+            ShowQuery()
+              setShowModal(!showModal);
+            }} title="Execute "
+    color="#fb5b5a"
+    accessibilityLabel="Learn more about this purple button"/> 
+  
+  
+    </View>
+      </View>
     :null
     
     }
@@ -244,6 +580,133 @@ const OrderBy=(props)=>{
 
 
 </ScrollView>
+
+
+{/* ---------------modal---------------- */}
+
+<Modal
+          animationType={'slide'}
+          transparent={false}
+          visible={showModal}
+          onRequestClose={() => {
+            console.log('Modal has been closed.');
+          }}>
+          <View style={styles.modal}>
+          <View>
+            <View style={{marginTop:10,marginBottom:10}}>
+              <Text>{query}</Text>
+            </View>
+  <Button 
+  onPress={()=>{
+    setShowModal(!showModal);
+    props.navigation.navigate('ExQuery',{
+      Query:query,
+      database:SelectedDatabase
+     } )
+  }
+} title="Execute Query"
+  color="#fb5b5a"
+  accessibilityLabel="Learn more about this purple button"/> 
+                   
+   </View>
+ 
+   <View style={{display:'flex',width:'100%',height:300}}>
+     <View style={styles.Mbtn}>
+              <Button 
+              title="Edit"
+              onPress={() => {
+                setShowModal(!showModal);
+              }}
+            />
+            </View>
+            <View style={styles.Mbtn}>
+              <Button style={{width:20}}
+              title="Save Query"
+              onPress={() => {
+               setInputModel(!inputModel)
+              }}
+            />
+            </View>
+            <View style={styles.Mbtn}>
+              <Button style={{width:20}}
+              title="Show Saved qery"
+              onPress={() => {
+               GetqueryFromDatabase()
+              }}
+            />
+            </View>
+            <View>
+          {
+            getSavedQuery.length>=1 ?
+            getSavedQuery.map((data,index)=>{
+              return(
+                <View key={index} style={{height:30,display:'flex',borderWidth:1}}>
+                  <TouchableOpacity
+                  onPress={()=>{
+                    setShowModal(!showModal);
+                props.navigation.push('ExQuery',{
+                  Query:data.Query,
+                  database:data.DatabaseName
+                })                            
+                  }}
+                  >
+                  <Text>
+                    {data.Query_Name}
+                  </Text>
+                  </TouchableOpacity>
+                  </View>
+              )
+            })
+
+            :null
+          }
+        </View>
+            </View>
+            </View>
+           <Button
+              title="Click To Close Modal"
+              onPress={() => {
+                setShowModal(!showModal);
+                console.log(showModal)
+                setResult('')
+              }}
+            /> 
+
+
+           
+        </Modal>
+        
+
+        <Modal
+          animationType={'slide'}
+          transparent={false}
+          visible={inputModel}
+          onRequestClose={() => {
+            console.log('Modal has been closed.');
+          }}>
+<View style={{height:400,justifyContent:'center'}}>
+<Text style={{fontSize:18,marginLeft:25}}>Enter Query Name </Text>
+  <TextInput 
+  style={{height:40,backgroundColor:'#fff',marginBottom:40,width:'80%',marginLeft:30}}
+  placeholder="Enter query Name"
+  value={Query_Name}
+  onChangeText={(Query_Name)=>setQuery_Name(Query_Name)}
+  />
+<Button
+              title="Save Query"
+              onPress={() => {
+                PostSavedQuery()
+                setInputModel(!inputModel);
+                
+              }}
+            />
+</View>
+
+          </Modal>
+
+
+
+
     </View>
     )
 }
